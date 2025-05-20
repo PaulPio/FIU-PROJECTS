e3393aeb61a5f7e67b04a8d7da45548c65d95ab9
PaulP <paulrpiotrowski@gmail.com>
2025-05-19 20:44:51 -0400

    finalized activity 1

--- src/util/Gradebook.java
+++ src/util/Gradebook.java
@@ -1,25 +1,41 @@
 package src.util;
-
+// Source code of the program to create the Gradebook object and it's functions
 public class Gradebook {
-    private Students[] listOfStudents;
+
+
+
+
+    // Creating variables
+    private Students[] listOfStudents; // Variable to store array of students
+
 
     public Gradebook(Students[] listOfStudents) {
+        /*Constructor of the gradebook object, it will request an array of Students, this means,
+        a list of students and their info */
+
         this.listOfStudents = listOfStudents;
     };
 
     public void tabulateStudentsInfo(){
-        System.out.println("First\tLast\tPID\tScore");
+        /* Function to print the students info */
+
+        System.out.printf("%-12s\t%-12s\t%-12s\t%-10s%n", "First","Last","PID","Score");
 
         for(Students student : listOfStudents){
-            System.out.printf("%-12s\t%-12s\t%-12d\t%-10d");
+            System.out.printf("%-12s\t%-12s\t%-12d\t%-10d%n", student.getFirstName(), student.getLastName(), student.getPantherID(), student.getScore());
         }
 
     }
 
 
-   // public double calculateAverageScore(){
-
-  //  }
+   public double calculateAverageScore(){
+        double avgscore = 0, score = 0;
+        for (int i = 0;i < listOfStudents.length; i++ ) {
+            score += listOfStudents[i].getScore();
+        }
+        avgscore = score / listOfStudents.length;
+        return avgscore;
+   }
 
     public static String convertToLetterGrade(double score){
         if(score >= 95){
@@ -50,5 +66,4 @@ public class Gradebook {
             return "F";
         }
     }
-
 }
