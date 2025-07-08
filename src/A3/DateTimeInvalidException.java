bae768eb64bd8908577d9dbff2a22898f4df6bbf
PaulP <paulrpiotrowski@gmail.com>
2025-07-07 20:46:09 -0400

    Started A3

---
+++ src/A3/DateTimeInvalidException.java
@@ -0,0 +1,20 @@
+package src.A3;
+
+
+public class DateTimeInvalidException extends Exception{
+    private DateTime invalidDateTime;
+
+    // Constructor of the exception
+    public DateTimeInvalidException(String message) {
+        super();
+        System.out.println(message);
+    }
+
+        public DateTimeInvalidException(DateTime invalidDateTime) {
+            this.invalidDateTime = invalidDateTime;
+        }
+        @Override
+        public String getMessage(){
+            return "Illegal Date/Time " + invalidDateTime + ".";
+        }
+}
---
+++ src/A3/InvalidDataTypeException.java
@@ -0,0 +1,19 @@
+package src.A3;
+
+/**
+ * Exception when the user types the wrong data type
+ */
+public class InvalidDataTypeException extends Exception {
+
+    private String message;
+
+    public InvalidDataTypeException(String message) {
+        super(message);
+    }
+
+  // Message to indicate the user what went wrong
+  @Override
+  public String getMessage(){
+    return "Illegal data type " + message + ".";
+  }
+}
