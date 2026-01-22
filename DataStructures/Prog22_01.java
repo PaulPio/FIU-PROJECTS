b85fec8c209ef596155b6e54efb3bd974dab0982
PaulPio <paulrpiotrowski@gmail.com>
2026-01-22 13:48:17 -0500

    changes and last class update

--- DataStructures/Prog22_01.java
+++ DataStructures/Prog22_01.java
@@ -6,9 +6,30 @@ public class Prog22_01 {
 
         System.out.println("Adding numbers 0 to 9 to the list...");
         for (int i = 0; i < 10; i++) {
-            list.add(i);
+            list.add((int)(Math.random()*10));
         }
 
+        System.out.println("Display new list: ");
+        list.display();
+        
+        int searchVal = 9;
+        System.out.println("Searching for " + searchVal + ": " + list.search(searchVal));
+
+
+        System.out.println("Removing 5...");
+        list.remove(5);
+        System.out.print("After removing 5: ");
+        list.display();
+
+        System.out.println("Inserting 100 at index 2...");
+        list.insert(100, 2);
+        System.out.print("After inserting 100: ");
+        list.display();
+
+        System.out.println("Removing item at index 0...");
+        list.removeItemAt(0);
+        System.out.print("After removing item at index 0: ");
+        list.display();
        
     }
 }
--- src/DF4/Q3/Comparable.java
+++
@@ -1,7 +0,0 @@
-package src.DF4.Q3;
-
-public interface Comparable<T>{
-    public int compareTo(T other);/*returns zero if this==other,
-     **returns a negative int if this < other,
-     **and returns a positive int otherwise*/
-}
