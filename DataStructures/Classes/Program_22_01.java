1a66ae71dd7f079249d6cbcb639d2c93dc2278bb
PaulPio <paulrpiotrowski@gmail.com>
2026-03-10 13:44:16 -0400

    class of 3-10-2026

---
+++ DataStructures/Classes/Program_22_01.java
@@ -0,0 +1,25 @@
+public class Program_22_01 {
+    public static void main(String[] args) {
+        new Program_22_01();
+    }
+
+    Program_22_01() {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(10);
+        bst.add(5);
+        bst.add(15);
+        bst.add(3);
+        bst.add(7);
+        bst.add(12);
+        bst.add(18);
+        bst.add(1); 
+        bst.display();
+        System.out.println(bst.getMin());
+        System.out.println(bst.contains(10));
+        System.out.println(bst.contains(20));
+        bst.display();
+        bst.preorderDisplay();
+        bst.postorderDisplay();
+        System.out.println(bst.toString());
+    }
+}
